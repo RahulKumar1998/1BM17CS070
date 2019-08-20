6db87ff56dca8d6aa7749f48b98293662505e9bb
RahulKumar1998 <50517820+RahulKumar1998@users.noreply.github.com>
2019-08-20 15:24:20 +0530

    Add files via upload

---
+++ lab2b.py
@@ -0,0 +1,21 @@
+import sys
+
+def rev1(str):
+	lis=str.split(" ")
+	lis.reverse()
+	print(lis)
+	a=" "
+	s2=a.join(lis)
+	print(s2)
+
+def rev2(str):
+	lis=str.split(" ")
+	lis2=" "
+	for i in lis:
+		lis2+=i[::-1]
+		lis2+=" "
+	print(lis2)
+
+str1=input("Enter the string")
+rev1(str1)
+rev2(str1)
